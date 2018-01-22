68c7ac7e58537bb8cd30d36073ab7880aeb1f5e1
Adam Washington <adam.washington@stfc.ac.uk>
2018-01-22 15:21:33 +0000

    Work in a morphism

--- Planner.hs
+++ Planner.hs
@@ -53,34 +53,55 @@ instance Eq Problem where
 instance Show Problem where
   show = show . problemInit
 
-data Tree = Branch Problem [(Action, Tree)]
+instance Recursive Problem where
+  project = build
+
+type instance Base Problem = Tree
+
+data Tree a = Branch Problem [(Action, a)]
   deriving (Show)
 
-root :: Tree -> Problem
+instance Functor Tree where
+  fmap f (Branch p as) = Branch p $ map (second f) as
+
+root :: Tree a -> Problem
 root (Branch r _) = r
 
-branches :: Tree -> [(Action, Tree)]
+branches :: Tree a -> [(Action, a)]
 branches (Branch _ b) = b
   
-build ::  Problem -> Tree
+build ::  Problem -> Tree Problem
 build p
   | isDone p = Branch p []
   | otherwise = Branch p $ zip actions branches
   where
     actions = filter (`actionPre` problemInit p) $ problemActions p
-    branches = map (build . \x -> p {problemInit = x}) $ map actionCall actions <*> pure (problemInit p)
+    branches = map (\x -> p {problemInit = x}) $ map actionCall actions <*> pure (problemInit p)
     -- branches = map (flip Branch [] . \x -> p {problemInit = x}) $ map actionCall actions <*> pure (problemInit p)
 
-bfs :: CommandList -> Tree -> [(CommandList, Problem)]
-bfs as tree = tbf [] [(as, tree)]
+-- collapse :: Tree (CommandList, Problem) -> (CommandList, Problem)
+-- collapse (Branch _ children) =
+--   (\(a, (cs, p)) -> (a:cs, p)) . head . filter (isDone . snd . snd) $ children
+
+-- bfs :: CommandList -> Tree Problem -> [(CommandList, Problem)]
+-- bfs as tree = tbf [] [(as, tree)]
+--   where
+--     tbf :: [World] -> [(CommandList, Tree Problem)] -> [(CommandList, Problem)]
+--     tbf seen qs = newheads ++ tbf (seen ++ map (problemInit . snd) newheads) (concatMap (annotate . second branches) qs)
+--       where
+--         actions = map fst qs
+--         annotate :: (CommandList, [(Action, Tree Problem)]) -> [(CommandList, Tree Problem)]
+--         annotate (cmds, ats) = map (first (:cmds)) ats
+--         newheads = filter ((`notElem` seen) . problemInit . snd) . map (second root) $ qs
+
+----
+
+collapse :: Tree [CommandList] -> [CommandList]
+collapse (Branch p as)
+  | isDone p = [[]]
+  | otherwise = concatMap go as
   where
-    tbf :: [World] -> [(CommandList, Tree)] -> [(CommandList, Problem)]
-    tbf seen qs = newheads ++ tbf (seen ++ map (problemInit . snd) newheads) (concatMap (annotate . second branches) qs)
-      where
-        actions = map fst qs
-        annotate :: (CommandList, [(Action, Tree)]) -> [(CommandList, Tree)]
-        annotate (cmds, ats) = map (first (:cmds)) ats
-        newheads = filter ((`notElem` seen) . problemInit . snd) . map (second root) $ qs
+    go (act, cmd) = map (act:) cmd
 
 myProblem n = Problem {
   problemInit = World [0..n] [] [],
@@ -88,8 +109,8 @@ myProblem n = Problem {
   problemActions = [
       move12,
       move13,
-      move21,
       move23,
+      move21,
       move31,
       move32
       ]
@@ -150,5 +171,6 @@ isDone :: Problem -> Bool
 isDone p = problemInit p == problemGoal p
 
 -- main = print . take 20 . bfs [] . build $ myProblem 0
-main = print . reverse . fst . head . dropWhile (not . isDone . snd) . bfs [] . build $ myProblem 4
+-- main = print . reverse . fst . head . dropWhile (not . isDone . snd) . bfs [] . build $ myProblem 4
 -- main = print . build $ myProblem 0
+main = print . head $ hylo collapse build $ myProblem 1
