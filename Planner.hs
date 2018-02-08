36489deac54b396cdfbd6904512088a39d33a236
Adam Washington <adam.washington@stfc.ac.uk>
2018-02-08 15:10:34 +0000

    Much, much, much better solver

--- Planner.hs
+++ Planner.hs
@@ -9,9 +9,11 @@ import Data.Foldable (minimumBy)
 import Data.Functor.Foldable
 import Data.Functor.Identity
 import Data.List (intercalate, nubBy, nub)
+import qualified Data.Set as S
 import qualified Data.Map.Strict as M
 import Data.Maybe (mapMaybe)
 import Debug.Trace
+import System.Environment (getArgs)
   
 
 -- type World = Integer
@@ -31,12 +33,12 @@ instance Show a => Show (State [World] a) where
   show = show . fst . flip runState []
 
 data World = World [Int] [Int] [Int]
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 data Action = Action {
   actionName :: String,
   actionPre :: World -> Bool,
   actionCall :: World -> World
-}
+  }
 type CommandList = [Action]
 
 instance Show Action where
@@ -54,11 +56,8 @@ instance Eq Problem where
 instance Show Problem where
   show = show . problemInit
 
-instance Recursive Problem where
-  project = build
-
-instance Corecursive Problem where
-  embed (Branch p _) = p
+instance Ord Problem where
+  a `compare` b = problemInit a `compare` problemInit b
 
 nextActions :: Problem -> [(Action, Problem)]
 nextActions p =
@@ -70,56 +69,9 @@ nextActions p =
   in
     filter ((`actionPre` (problemInit p)) . fst) . zip actions $ paths
 
-type instance Base Problem = Tree
-
-data Tree a = Branch Problem [(Action, a)]
-  deriving (Show)
-
-instance Functor Tree where
-  fmap f (Branch p as) = Branch p $ map (second f) as
-
-root :: Tree a -> Problem
-root (Branch r _) = r
-
-branches :: Tree a -> [(Action, a)]
-branches (Branch _ b) = b
-  
-build ::  Problem -> Tree Problem
-build p
-  | isDone p = Branch p []
-  | otherwise = Branch p $ zip actions branches
-  where
-    actions = filter (`actionPre` problemInit p) $ problemActions p
-    branches = map (\x -> p {problemInit = x}) $ map actionCall actions <*> pure (problemInit p)
-    -- branches = map (flip Branch [] . \x -> p {problemInit = x}) $ map actionCall actions <*> pure (problemInit p)
-
--- collapse :: Tree (CommandList, Problem) -> (CommandList, Problem)
--- collapse (Branch _ children) =
---   (\(a, (cs, p)) -> (a:cs, p)) . head . filter (isDone . snd . snd) $ children
-
--- bfs :: CommandList -> Tree Problem -> [(CommandList, Problem)]
--- bfs as tree = tbf [] [(as, tree)]
---   where
---     tbf :: [World] -> [(CommandList, Tree Problem)] -> [(CommandList, Problem)]
---     tbf seen qs = newheads ++ tbf (seen ++ map (problemInit . snd) newheads) (concatMap (annotate . second branches) qs)
---       where
---         actions = map fst qs
---         annotate :: (CommandList, [(Action, Tree Problem)]) -> [(CommandList, Tree Problem)]
---         annotate (cmds, ats) = map (first (:cmds)) ats
---         newheads = filter ((`notElem` seen) . problemInit . snd) . map (second root) $ qs
-
-build2 :: Problem -> Tree Problem
-build2 p = Branch p $ nextActions p
-
 ----
 
-collapse :: Tree [CommandList] -> [CommandList]
-collapse (Branch p as)
-  | isDone p = [[]]
-  | otherwise = concatMap go as
-  where
-    go (act, cmd) = map (act:) cmd
-
+myProblem :: Int -> Problem
 myProblem n = Problem {
   problemInit = World [0..n] [] [],
   problemGoal = World [] [] [0..n],
@@ -132,77 +84,91 @@ myProblem n = Problem {
       move32
       ]
   }
+move12 :: Action
 move12 = Action {
   actionName = "move top of 1 to 2",
     actionPre = move12',
     actionCall = \(World (a:as) bs cs) -> World as (a:bs) cs
   }
+move13 :: Action
 move13 = Action {
   actionName = "move top of 1 to 3",
     actionPre = move13',
     actionCall = \(World (a:as) bs cs) -> World as bs (a:cs)
   }
+move21 :: Action
 move21 = Action {
   actionName = "move top of 2 to 1",
     actionPre = move21',
     actionCall = \(World as (b:bs) cs) -> World (b:as) bs cs
   }
+move23 :: Action
 move23 = Action {
   actionName = "move top of 2 to 3",
     actionPre = move23',
     actionCall = \(World as (b:bs) cs) -> World as bs (b:cs)
   }
+move31 :: Action
 move31 = Action {
   actionName = "move top of 3 to 1",
     actionPre = move31',
     actionCall = \(World as bs (c:cs)) -> World (c:as) bs cs
   }
+move32 :: Action
 move32 = Action {
   actionName = "move top of 3 to 2",
     actionPre = move32',
     actionCall = \(World as bs (c:cs)) -> World as (c:bs) cs
   }
 
-move12' (World (a:as) [] _) = True
-move12' (World (a:as) (b:bs) _) = a < b
+type ActionPre = (World -> Bool)
+
+move12' :: ActionPre
+move12' (World (_:_) [] _) = True
+move12' (World (a:_) (b:_) _) = a < b
 move12' _ = False
-move13' (World (a:as) _ []) = True
-move13' (World (a:as) _ (b:bs)) = a < b
+
+move13' :: ActionPre
+move13' (World (_:_) _ []) = True
+move13' (World (a:_) _ (b:_)) = a < b
 move13' _ = False
 
-move21' (World [] (b:bs) _) = True
-move21' (World (a:as) (b:bs) _) = b < a
+move21' :: ActionPre
+move21' (World [] (_:_) _) = True
+move21' (World (a:_) (b:_) _) = b < a
 move21' _ = False
-move23' (World _ (b:bs) []) = True
-move23' (World _ (b:bs) (c:cs)) = b < c
+
+move23' :: ActionPre
+move23' (World _ (_:_) []) = True
+move23' (World _ (b:_) (c:_)) = b < c
 move23' _ = False
 
-move31' (World [] _ (c:cs)) = True
-move31' (World (a:as) _ (c:cs)) = c < a
+move31' :: ActionPre
+move31' (World [] _ (_:_)) = True
+move31' (World (a:_) _ (c:_)) = c < a
 move31' _ = False
-move32' (World _ [] (c:cs)) = True
-move32' (World _ (b:bs) (c:cs)) = c < b
+
+move32' :: ActionPre
+move32' (World _ [] (_:_)) = True
+move32' (World _ (b:_) (c:_)) = c < b
 move32' _ = False
 
 isDone :: Problem -> Bool
 isDone p = problemInit p == problemGoal p
 
-memohylo :: (Base Problem b -> b) -> (a -> Base Problem a) -> a -> b
-memohylo down up x = chrono down' up' $ ([], x)
-  where
-    -- up' :: a -> Base Problem (Free (Base Problem) a)
-    up' (seen, v) =
-      let
-        Branch p as = up v
-      in
-        if p `elem` seen
-        then Branch p []
-        else Branch p $ map (second (return . (\x -> (p:seen, x)))) as
-    -- down' :: (Base Problem) (Cofree (Base Problem) b) -> b
-    down' (Branch p as) = down (Branch p $ map (second extract) as)
+solve' :: [(CommandList, Problem)] -> State (S.Set Problem) [(CommandList, Problem)]
+solve' [] = return []
+solve' xs = do
+  st <- get
+  -- trace (show xs) $ return ()
+  let go (cs, p) = [(n:cs, p') | (n, p') <- nextActions p]
+  let next = filter (\x -> snd x `S.notMember` st) $ concatMap go xs
+  modify (S.union (S.fromList $ map snd next))
+  promise <- solve' next
+  return $ xs ++ promise
 
+solve :: Problem -> CommandList
+solve p = fst . head . filter (isDone . snd) . flip evalState (S.singleton p) $ solve' [([], p)]
 
--- main = print . take 20 . bfs [] . build $ myProblem 0
--- main = print . reverse . fst . head . dropWhile (not . isDone . snd) . bfs [] . build $ myProblem 4
--- main = print . build $ myProblem 0
-main = print . head $ memohylo collapse build2 $ myProblem 2
+main :: IO ()
+main = getArgs >>= print . length . solve . myProblem . read . head
